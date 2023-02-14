a50cc80a0308f6aa485f8a4f0f9d4840fb0ad547
Nizx <76437265+nizxcoder@users.noreply.github.com>
2023-02-14 16:40:59 +0530

    news api change so we can fetch from any origin

--- src/App.js
+++ src/App.js
@@ -38,14 +38,14 @@ export default class App extends Component {
                     />
                     <Navbar />
                     <Routes>
-                        <Route path={'/'} element={<News setProgress={this.setProgress} apiKey={this.apiKey} key={query} category={query || "Home"} />}></Route>
-                        <Route exact path='/sports' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/sports' category="sports" />} />
-                        <Route exact path='/politics' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/politics' category="politics" />} />
-                        <Route exact path='/business' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/business' category="business" />} />
-                        <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/entertainment' category="entertainment" />} />
-                        <Route exact path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/science' category="science" />} />
-                        <Route exact path='/technology' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/technology' category="technology" />} />
-                        <Route exact path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key='/health' category="health" />} />
+                        <Route path={'/'} element={<News setProgress={this.setProgress} key='general' apiKey={this.apiKey} query={query} category={"general"} />}></Route>
+                        <Route exact path='/sports' element={<News setProgress={this.setProgress} query={query} apiKey={this.apiKey} key='/sports' category="sports" />} />
+                        <Route exact path='/politics' element={<News setProgress={this.setProgress} query={query} apiKey={this.apiKey} key='/politics' category="politics" />} />
+                        <Route exact path='/business' element={<News setProgress={this.setProgress} query={query} apiKey={this.apiKey} key='/business' category="business" />} query={query} />
+                        <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} query={query} key='/entertainment' category="entertainment" />} />
+                        <Route exact path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} query={query} key='/science' category="science" />} />
+                        <Route exact path='/technology' element={<News setProgress={this.setProgress} apiKey={this.apiKey} query={query} key='/technology' category="technology" />} />
+                        <Route exact path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} query={query} key='/health' category="health" />} />
                     </Routes>
                 </div>
             </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -8,6 +8,7 @@ export default class News extends Component {
     static defaultProps = {
         country: 'in',
         category: 'general',
+
     }
     static propTypes = {
         country: PropTypes.string,
@@ -20,40 +21,44 @@ export default class News extends Component {
             articles: [],
             loading: false,
             pageNo: 1,
-            totalResult: 0
+            totalResult: 0,
+            temp: '',
         }
         document.title = props.category.charAt(0).toUpperCase() + this.props.category.slice(1).replace('+', ' ')+" - Top Headlines";
     }
     async updateNews() {
+        // console.log(this.props.query)
         this.props.setProgress(10);
-        const url = `https://newsapi.org/v2/everything?q=${this.props.category}&pageSize=15&page=${this.state.pageNo}&language=en&sortBy=publishedAt&apiKey=${this.props.apiKey}`;
+        // const url = `https://newsapi.org/v2/everything?q=${this.props.category}&pageSize=15&page=${this.state.pageNo}&language=en&sortBy=publishedAt&apiKey=${this.props.apiKey}`;
+        const url = `http://api.mediastack.com/v1/news?access_key=d8c3fa48c9e4127379a55333b429b52e&keywords=${this.props.query}&categories=${this.props.category}&keyword=sort=published_desc&limit=15&languages=en&offset=${this.state.pageNo+1}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let newsData = await data.json();
         this.props.setProgress(60);
         this.setState({
-            articles: newsData.articles,
+            articles: newsData.data,
             loading: false,
-            totalResult: newsData.totalResults
+            totalResult: newsData.pagination.totalResult
         })
         this.props.setProgress(100);
-
     }
     async componentDidMount() {
         this.updateNews();
     }
     fetchMoreData = async () => {
-        const url = `https://newsapi.org/v2/everything?q=${this.props.category}&pageSize=15&page=${this.state.pageNo+1}&language=en&sortBy=publisheAt&apiKey=${this.props.apiKey}`;
+        // const url = `https://newsapi.org/v2/everything?q=${this.props.category}&pageSize=15&page=${this.state.pageNo+1}&language=en&sortBy=publisheAt&apiKey=${this.props.apiKey}`;
+        const url = `http://api.mediastack.com/v1/news?access_key=d8c3fa48c9e4127379a55333b429b52e&keywords=${this.props.query}&categories${this.props.category}&sort=published_desc&limit=15&languages=en&offset=${this.state.pageNo+1}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let newsData = await data.json();
         setTimeout(() => {
             this.setState({
-                articles: this.state.articles.concat(newsData.articles),
+                articles: this.state.articles.concat(newsData.data),
                 loading: false,
                 pageNo: this.state.pageNo + 1
             })
         }, 500);
+        console.log(this.state.pageNo)
     }
     render() {
         return (
@@ -65,14 +70,14 @@ export default class News extends Component {
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
                     next={this.fetchMoreData}
-                    hasMore={90 >  this.state.articles.length}
+                    hasMore={this.state.totalResult !== this.state.articles.length}
                     loader={<Spinner/>}
                     >
                     <div className="d-flex flex-wrap">
                         {
                             this.state.articles.map((element) => {
                                 return (
-                                    <NewsItems key={element.url} url={element.url} name={element.source.name.slice(0, 20)} author={(element.author && element.author.slice(0,20)) || "Unknown"} title={element.title.slice(0, 60)} published={new Date(element.publishedAt).toGMTString()} description={element.description && element.description.slice(0, 90)} imageUrl={element.urlToImage} />
+                                    <NewsItems key={element.url} url={element.url} name={element.source.slice(0, 20)} author={(element.author && element.author.slice(0,20)) || "Unknown"} title={element.title.slice(0, 60)} published={new Date(element.published_at).toGMTString()} description={element.description && element.description.slice(0, 90)} imageUrl={element.image} />
                                 )
                             })
                         }
